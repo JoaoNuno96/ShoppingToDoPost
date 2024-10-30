1da839bc22689f1e3367c996350c154ee1fc479c
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-10-30 22:04:47 +0000

    Implement FireBase. Shopping List Recovers and deletes data from firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,7 +47,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.cronet.embedded)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-firestore-ktx");
+    implementation("com.google.firebase:firebase-auth-ktx");
 }
--- app/src/main/java/com/example/listacompras/views/ShoppingListActivity.kt
+++ app/src/main/java/com/example/listacompras/views/ShoppingListActivity.kt
@@ -1,49 +1,52 @@
 package com.example.listacompras.views
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listacompras.adapter.ShoppingListAdapter
-import com.example.listacompras.data.ListaMock
 import com.example.listacompras.databinding.ActivityShoppingListBinding
 import com.example.listacompras.model.ShoppingItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ShoppingListActivity : AppCompatActivity() {
 
     private val binding by lazy{
         ActivityShoppingListBinding.inflate(layoutInflater);
     }
-    public val lista = ArrayList<ShoppingItem>();
-    public val listaShared : Map<Int,String> = mapOf();
-    public var listSize = lista.size;
+
+    private val auth by lazy{
+        FirebaseAuth.getInstance();
+    }
+
+    private val db by lazy{
+        FirebaseFirestore.getInstance();
+    }
+
+    public var lista = ArrayList<ShoppingItem>();
+    public var listaExemplo : ArrayList<String> = ArrayList<String>();
+
+    public val shoppingItem : ShoppingItem? = null;
     public var flagVariable = false;
+    public var recover : Map<String,Any>? = null;
     public var count = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(binding.root);
 
-        val mock = ListaMock();
-
-        binding.recycleView.layoutManager = LinearLayoutManager(this);
-        binding.recycleView.adapter = ShoppingListAdapter(lista);
-
-        val sharedPreferences = this.getSharedPreferences("lista", Context.MODE_PRIVATE);
-        val valor = sharedPreferences.getString("","").toString();
-
-        if(flagVariable)
-        {
-            verificarShared(valor);
-        }
-
+        loadData();
 
         binding.botaoAdicionar.setOnClickListener {
 
             val adicionarItem = binding.textoAdicionar.text.toString();
 
-            adicionarItems(adicionarItem,sharedPreferences);
+            val userId = auth.currentUser?.uid;
+
+            adicionarItems(adicionarItem,userId.toString());
 
             binding.textoAdicionar.setText("");
         }
@@ -58,36 +61,100 @@ class ShoppingListActivity : AppCompatActivity() {
         }
     }
 
-    fun adicionarItems(item : String, sp : SharedPreferences) {
+    private fun adicionarItems(item : String, userId : String) {
 
         //ADICIONAR LISTA
         val shop_item = ShoppingItem(count,item)
         lista.add(shop_item);
         count++;
 
-        flagVariable = true;
-        //ADICIONAR ADAPTER LIST VIEW
-//        val arrayAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, lista);
-//        binding.listaDeItems.adapter = arrayAdapter;
+        //MANDA BASE DE DADOS
+        var data = mapOf(
+            shop_item.id.toString() to shop_item.nome
+        );
+
+        db.collection(userId).document("listaTarefas").update(data);
+
+        binding.recycleView.layoutManager = LinearLayoutManager(this);
         binding.recycleView.adapter = ShoppingListAdapter(lista);
 
-        //ADICIONAR SHARED (EDITOR)
-        val editor : SharedPreferences.Editor = sp.edit();
-        editor.putString("${shop_item.id}","${shop_item.nome}");
-        editor.apply();
     }
 
+    private fun adicionarItemsRecuperadosDeDB(item : String, userId : String)
+    {
+        //ADICIONAR LISTA
+        val shop_item = ShoppingItem(count,item)
+        lista.add(shop_item);
+        count++;
+
+
+        //MANDA BASE DE DADOS
+        var data = mapOf(
+            shop_item.id.toString() to shop_item.nome
+        );
+
+        db.collection(userId).document("listaTarefas").update(data);
+
+    }
 
     fun removerItem(index : Int) {
 
+        //REMOVER DA LISTA
         var item = lista.get(index-1);
         lista.remove(item);
 
+        //REMOVER DB
+        itemRemoveFromDataBase(index.toString());
+
+        //REORDENAR
         verificar();
 
-//        val arrayAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, lista);
-//        binding.listaDeItems.adapter = arrayAdapter;
+        binding.recycleView.layoutManager = LinearLayoutManager(this);
         binding.recycleView.adapter = ShoppingListAdapter(lista);
+    }
+
+    //REMOVER DADOS PELA BASE DE DADOS CONSOANTE O ID DO FIELD
+    fun itemRemoveFromDataBase(userData : String)
+    {
+        val userId = auth.currentUser?.uid;
+
+        val documentRef = db.collection(userId.toString()).document("listaTarefas");
+
+        var removesDatas = hashMapOf<String,Any>(
+            userData to FieldValue.delete(),
+            )
+
+        documentRef.update(removesDatas).addOnCompleteListener {
+            Toast.makeText(applicationContext, "Remove Success", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun loadData()
+    {
+        val userId = auth.currentUser?.uid;
+
+        db.collection(userId.toString()).document("listaTarefas").get()
+            .addOnSuccessListener {
+
+                recover = it.getData();
+//                Thread.sleep(4000);
+
+                recover?.forEach { entry ->
+
+                    adicionarItemsRecuperadosDeDB(entry.value.toString(),userId.toString());
+                }
+
+           }
+
+        Toast.makeText(applicationContext, "Dados a carregar de base de dados...", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({
+            binding.recycleView.layoutManager = LinearLayoutManager(this);
+            binding.recycleView.adapter = ShoppingListAdapter(lista);
+
+        },3000)
+
     }
 
     fun verificar(){
@@ -96,23 +163,7 @@ class ShoppingListActivity : AppCompatActivity() {
             lista.get(i).id = lista.indexOf(lista.get(i)).toString().toInt() +1;
         }
 
-//        val arrayAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, lista);
-//        binding.listaDeItems.adapter = arrayAdapter;
-        binding.recycleView.adapter = ShoppingListAdapter(lista);
-    }
-
-    fun verificarShared(valor : String){
-
-        valor.toString()
-
-        //ADICIONAR LISTA
-        val shop_item = ShoppingItem(count,valor)
-        lista.add(shop_item);
-        count++;
-
-        //ADICIONAR ADAPTER LIST VIEW
-//        val arrayAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, lista);
-//        binding.listaDeItems.adapter = arrayAdapter;
+        binding.recycleView.layoutManager = LinearLayoutManager(this);
         binding.recycleView.adapter = ShoppingListAdapter(lista);
     }
 
